194f57741647c3c966dd7522df88cbbf8991348e
Hamuul <luca.pana@stud.acs.upb.ro>
2024-02-21 21:58:57 +0200

    scraper works but is slow. blame selenium

--- sel.py
+++ sel.py
@@ -1,30 +1,67 @@
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.common.by import By
+import asyncio
+import json
+from itertools import cycle
 
 from fake_useragent import UserAgent
+from lxml import html
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+from assemble_product_urls import convert_xmls_to_prodlink_generator as convert
 
 fopt = FirefoxOptions()
 ua = UserAgent()
 
-
 profile = webdriver.FirefoxProfile()
 profile.set_preference("general.useragent.override", ua.random)
+profile.set_preference("permissions.default.stylesheet", 2)
+profile.set_preference("permissions.default.image", 2)
+profile.set_preference("javascript.enabled", False)
+profile.set_preference("layers.acceleration.disabled", True)
+profile.set_preference("network.http.pipelining", True)
+profile.set_preference("network.prefetch-next", False)
 fopt.profile = profile
+fopt.page_load_strategy = 'eager'
 fopt.add_argument("--headless")
 
-driver = webdriver.Firefox(options=fopt)
-# driver.get('https://shop.rewe.de/sitemaps/sitemap-shop-produkte.xml')
-# product_links = driver.find_elements(By.TAG_NAME, 'loc')
 
 
-# gen = (link.text for link in product_links[:1])
+NUM_DRIVERS = 4
+drivers = [webdriver.Firefox(options=fopt) for _ in range(NUM_DRIVERS)]
 
-# for url in gen:
-driver.get("https://shop.rewe.de/p/all-e-bleue-cool-hills-weisswein-sauvignon-blanc-trocken-0-75l/1454443")
-sleep(2)
-full_json = driver.find_element(By.CSS_SELECTOR, '[id^="pdpr-propstore"]').get_attribute('textContent')
-print(full_json)
-driver.quit()
 
+def extract_desired_fields(url, driver):
+    driver.get(url)
+    try:
+        tree = html.fromstring(
+            driver.find_element(By.CSS_SELECTOR, "#pdpr-ProductInformation").get_attribute("innerHTML"))
+        price_element = tree.xpath('//meso-data[@data-context="product-detail"]')[0]
+        price = price_element.get('data-price')
+
+        json_string = driver.find_element(By.CSS_SELECTOR, '[id^="pdpr-propstore"]').get_attribute('textContent')
+        json_dict = json.loads(json_string)
+        json_dict = json_dict["productData"]
+    except NoSuchElementException:
+        return
+    try:
+        print(
+            f'{json_dict["productName"]}\n{json_dict["brandKey"]}\n{price}eur\n'
+            f'{json_dict["gtin"]}\n')
+    except KeyError:
+        idx = json_dict["slug"].find("-")
+        print(
+            f'{json_dict["productName"]}\n{json_dict["slug"][:idx]}\n{price}eur\n'
+            f'{json_dict["gtin"]}\n')
+
+
+async def main():
+    idx = 0
+    for link, driver in zip(await convert(["extracted0.xml", "extracted1.xml"]), cycle(drivers)):
+        extract_desired_fields(link, driver[idx])
+        idx += 1
+        idx %= NUM_DRIVERS
+
+
+asyncio.run(main())
