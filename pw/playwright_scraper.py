b7c4496a34ed3077fbf39d8fadd75682acdc98e9
Hamuul <luca.pana@stud.acs.upb.ro>
2024-02-22 22:00:00 +0200

    do 1 context per task. playwright otherwise has a bug where it consumes unbounded heap memory and fills RAM

--- pw/playwright_scraper.py
+++ pw/playwright_scraper.py
@@ -3,18 +3,15 @@ import os
 import asyncio
 from functools import partial
 
-
 import uvloop
 import pypeln as pl
 import ujson
 import aiofiles
 
-
 from fake_useragent import UserAgent
 from playwright.async_api import async_playwright
 from lxml import html
 from aiocsv import AsyncWriter
-
 
 from assemble_product_urls import convert_xmls_to_iterable as convert
 
@@ -45,7 +42,7 @@ async def extract_desired_field(url, page):
         if price.startswith("."):
             price = f"0{price}"
 
-        async with aiofiles.open("products.csv", mode="a", encoding="utf-8", newline="") as afp:
+        async with aiofiles.open("full.csv", mode="a", encoding="utf-8", newline="") as afp:
             writer = AsyncWriter(afp, dialect="unix")
             try:
                 await writer.writerows(
@@ -75,12 +72,16 @@ async def extract_desired_field(url, page):
         return None
 
 
-async def task(context, link_gen):
-    page = await context.new_page()
+async def task(browser, link_gen):
     for link in link_gen:
         try:
+            context = await browser.new_context(
+                java_script_enabled=False, user_agent=UserAgent().random
+            )
+            page = await context.new_page()
             await page.route("**/*.{png,jpg,jpeg,svg}", lambda route: route.abort())
             await extract_desired_field(link, page)  # Pass the page to the function
+            await context.close()
         except Exception as e:
             print(f"Error processing {link}: {e}")
             await page.close()
@@ -89,8 +90,6 @@ async def task(context, link_gen):
 
 async def main():
     link_gen = await convert(["extracted0.xml", "extracted1.xml"])
-    if not os.path.exists("products.csv"):
-        open("products.csv", "w").write("productName,brand,price,GTIN\n")
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(
@@ -104,20 +103,17 @@ async def main():
                 "--use-angle=vulkan",
             ],
         )
-        context = await browser.new_context(
-            java_script_enabled=False, user_agent=UserAgent().random
-        )
 
         chunked_list = list()
         num_links = len(link_gen)
         chunk_size = 8096
 
         for i in range(0, num_links, chunk_size):
-            chunked_list.append(link_gen[i : i + chunk_size])
+            chunked_list.append(link_gen[i: i + chunk_size])
         await asyncio.gather(
-            *[task(context, chunked_list[i]) for i in range(len(chunked_list))]
+            *[task(browser, chunked_list[i]) for i in range(len(chunked_list))]
         )
-        # stage = pl.task.map(partial(task, context), chunked_list, workers=12)
+        # stage = pl.task.map(partial(task, context), chunked_list, workers=6)
         # await stage
 
 
