3b94ccaa5f11c9ec73687acf21760c8513ff2c13
Hamuul <luca.pana@stud.acs.upb.ro>
2024-02-22 15:00:50 +0200

    cleanup comment

--- pw/assemble_product_urls.py
+++ pw/assemble_product_urls.py
@@ -1,19 +1,13 @@
 from xmltodict import parse
 import aiofiles
 
-async def convert_xmls_to_prodlink_generator(filenames):
+
+async def convert_xmls_to_iterable(filenames):
     xml_content = []
     for i, file in enumerate(filenames):
-        async with aiofiles.open(file, 'r') as f:
+        async with aiofiles.open(file, "r") as f:
             xml_content.append(parse(await f.read()))
-    product_link_gen = [subpart["loc"] for part in xml_content
-                                        for subpart in part["urlset"]["url"]]
+    product_link_gen = [
+        subpart["loc"] for part in xml_content for subpart in part["urlset"]["url"]
+    ]
     return product_link_gen
-
-
-### sanity test
-# async def main():
-#   for link in await convert_xmls_to_prodlink_generator(["extracted0.xml", "extracted1.xml"]):
-#       print(link)
-#
-# asyncio.run(main())
